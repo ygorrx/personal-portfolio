8f788490cfe7625b37f95a474def74eca1009c9e
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-12 16:44:32 -0300

    preload configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
 import '../styles/globals.css'
+import React from 'react'
 import Layout from './components/Layout'
+import Loading from './components/Loading'
 
 function MyApp({ Component, pageProps }) {
+
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    setTimeout(() => setLoading(false), 1200)
+  }, []); 
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+    <>
+     {loading ? (
+       <Loading/>
+        
+      ) : (
+        <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      )}
+  </>
+    )
+ }
+
 
 export default MyApp
---
+++ pages/components/Loading.js
@@ -0,0 +1,20 @@
+
+import styles from '../../styles/Loading.module.css'
+import Image from 'next/image'
+
+const Loading = () => {
+  return (
+    <div className={styles.container}>
+    <div>
+    <Image
+                src="/assets/logo-animate.gif"
+                width="150"
+                height="150"
+                alt="logo"
+    ></Image>
+    </div>
+    </div>
+  )
+}
+
+export default Loading
