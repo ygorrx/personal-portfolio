10ae8aecf45f0b9be864b36eb16780b360c6b1e2
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-14 19:42:21 -0300

    animation scroll + theme button

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,52 +1,104 @@
+import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../../styles/Navbar.module.css'
+import { motion, useViewportScroll } from 'framer-motion'
+import { IconContext } from 'react-icons'
+import { FaMoon, FaSun } from 'react-icons/fa'
 
-const Navbar = ({ theme }) => {
+const Navbar = ({ theme, setTheme }) => {
+  const { scrollY } = useViewportScroll()
+
+  /** this hook manages state **/
+  const [hidden, setHidden] = React.useState(false)
+
+  function update() {
+    if (scrollY?.current < scrollY?.prev) {
+      setHidden(false)
+    } else if (scrollY?.current > 30 && scrollY?.current > scrollY?.prev) {
+      setHidden(true)
+    }
+  }
+
+  /** update the onChange callback to call for `update()` **/
+  React.useEffect(() => {
+    return scrollY.onChange(() => update())
+  })
+
+  /** add this const **/
+  const variants = {
+    /** this is the "visible" key and it's correlating styles **/
+    visible: { opacity: 1, y: 0 },
+    /** this is the "hidden" key and it's correlating styles **/
+    hidden: { opacity: 0, y: -25 }
+  }
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.nav}>
-        {theme === 'light' ? (
-          <Link href="/">
-            <div className={styles.link_home}>
-              <Image
-                src="/assets/ym-logo.svg"
-                width="103"
-                height="103"
-                alt="logo"
-              ></Image>
-            </div>
-          </Link>
-        ) : (
-          <Link href="/">
-            <div className={styles.link_home}>
-              <Image
-                src="/assets/ym-logo-white.svg"
-                width="103"
-                height="103"
-                alt="logo"
-              ></Image>
-            </div>
-          </Link>
-        )}
-        <ul className={styles.link_items}>
-          <li>
-            <Link href="/Projects">
-              <a>Projects</a>
+      <motion.nav
+        variants={variants}
+        animate={hidden ? 'hidden' : 'visible'}
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
+      >
+        <div className={styles.nav}>
+          {theme === 'light' ? (
+            <Link href="/">
+              <div className={styles.link_home}>
+                <Image
+                  src="/assets/ym-logo.svg"
+                  width="103"
+                  height="103"
+                  alt="logo"
+                ></Image>
+              </div>
             </Link>
-          </li>
-          <li>
-            <Link href="/About">
-              <a>About</a>
+          ) : (
+            <Link href="/">
+              <div className={styles.link_home}>
+                <Image
+                  src="/assets/ym-logo-white.svg"
+                  width="103"
+                  height="103"
+                  alt="logo"
+                ></Image>
+              </div>
             </Link>
-          </li>
-          <li>
-            <Link href="/Contact">
-              <a>Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
+          )}
+          <ul className={styles.link_items}>
+            <li>
+              <Link href="/Projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/About">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/Contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+            <button className={styles.button} onClick={switchTheme}>
+              {theme === 'light' ? (
+                <IconContext.Provider value={{ size: '25px', color: 'black' }}>
+                  <FaMoon />
+                </IconContext.Provider>
+              ) : (
+                <IconContext.Provider value={{ size: '25px', color: 'white' }}>
+                  <FaSun />
+                </IconContext.Provider>
+              )}
+            </button>
+          </ul>
+        </div>
+      </motion.nav>
     </nav>
   )
 }
