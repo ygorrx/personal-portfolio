9ea206e3041608e746671b888a436187d8023667
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-17 16:51:20 -0300

    Button Changes

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,8 +1,10 @@
+import React from 'react'
 import styles from '../../styles/Button.module.css'
+import { useRouter } from 'next/router'
 
 const Button = ({ children, ...props }) => {
   return (
-    <button {...props} className={`${styles.button} ${styles.btn}`}>
+    <button onClick={() => router.push("/page")} {...props} className={`${styles.button} ${styles.btn}`}>
       <span>
         <a>{children}</a>
       </span>
