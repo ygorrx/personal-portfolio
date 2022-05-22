6c60200d4f6c5d874d1c923c7d93fcd4ce32576d
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-21 23:45:34 -0300

    i18n config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.imgur.com']
-  }
+  },
+  i18n
 }
 
 module.exports = nextConfig
