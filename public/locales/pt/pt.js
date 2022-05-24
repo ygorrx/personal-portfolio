ca59d45f1574365c23953d4505811f1bd5ca2f6a
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-23 21:55:54 -0300

    i18n thankpage

--- public/locales/pt/pt.js
+++ public/locales/pt/pt.js
@@ -10,18 +10,24 @@ export default {
   homep1: 'Apaixonado por tecnologia e suas possibilidades.',
   footer1: 'Projetado e desenvolvido com amor por',
   about1: 'sobre mim',
-about2: 'Pequenos fatos sobre mim.',
-about_description1: 'Olá, meu nome é',
-about_description2: ', e eu sou do Brasil. Eu sou Bacharel em Sistemas e Mídias Digitais pela Universidade Federal do Ceará. Sempre fui apaixonado por tecnologia e suas possibilidades. Eu sempre fui fascinado por artes e sempre quis saber como as coisas funcionavam, como tudo que eu via na internet, na TV e em publicações eram feitas. Foi assim que eu comecei a apreciar design. Com a introdução de nova tecnologia, meu interesse no mundo web cresceu com o tempo. Atualmente, eu estou em busca de uma transição de carreira. Apesar de ter estudado desenvolvimento na faculdade, decidi atuar na área de design criativo e marketing, consolidando assim boas experiências com aspectos visuais, criação de interface e experiência do usuário. Agora estou voltando para a área de desenvolvimento e não vi solução melhor do que empregar essas experiências adquiridas na área que mais me identifico,',
-about_description3: 'Desenvolvimento Web Frontend',
-about_description4: 'Sou uma pessoa extremamente dedicada, curiosa, disposta a fazer sempre o melhor do que me é pedido. Se não sei de algo, eu vou atrás de aprender.',
-about_techs: 'Ferramentas e tecnologias que eu uso',
-about_techs_p: 'Design Criativo',
-about_techs_p_2: 'Desenvolvimento',
-about_quote: '"Os dois mais importantes dias da sua vida são os dias que você nasce e o dia que você descobre o porquê."',
-about_resume: 'Baixe meu currículo',
-about_button: 'Clique aqui!',
-project1: 'projetos',
-project2: 'Uma coleção dos meus trabalhos recentes.',
-project_button: 'Veja o projeto!'
+  about2: 'Pequenos fatos sobre mim.',
+  about_description1: 'Olá, meu nome é',
+  about_description2:
+    ', e eu sou do Brasil. Eu sou Bacharel em Sistemas e Mídias Digitais pela Universidade Federal do Ceará. Sempre fui apaixonado por tecnologia e suas possibilidades. Eu sempre fui fascinado por artes e sempre quis saber como as coisas funcionavam, como tudo que eu via na internet, na TV e em publicações eram feitas. Foi assim que eu comecei a apreciar design. Com a introdução de nova tecnologia, meu interesse no mundo web cresceu com o tempo. Atualmente, eu estou em busca de uma transição de carreira. Apesar de ter estudado desenvolvimento na faculdade, decidi atuar na área de design criativo e marketing, consolidando assim boas experiências com aspectos visuais, criação de interface e experiência do usuário. Agora estou voltando para a área de desenvolvimento e não vi solução melhor do que empregar essas experiências adquiridas na área que mais me identifico,',
+  about_description3: 'Desenvolvimento Web Frontend',
+  about_description4:
+    'Sou uma pessoa extremamente dedicada, curiosa, disposta a fazer sempre o melhor do que me é pedido. Se não sei de algo, eu vou atrás de aprender.',
+  about_techs: 'Ferramentas e tecnologias que eu uso',
+  about_techs_p: 'Design Criativo',
+  about_techs_p_2: 'Desenvolvimento',
+  about_quote:
+    '"Os dois mais importantes dias da sua vida são os dias que você nasce e o dia que você descobre o porquê."',
+  about_resume: 'Baixe meu currículo',
+  about_button: 'Clique aqui!',
+  project1: 'projetos',
+  project2: 'Uma coleção dos meus trabalhos recentes.',
+  project_button: 'Veja o projeto!',
+  thank1: 'Obrigado por entrar em contato!',
+  thank2: 'Em breve retornarei uma resposta para seu e-mail.',
+  thank3: '← Voltar para a página principal'
 }
