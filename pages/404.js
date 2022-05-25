38b1ef8066733ec8426a05ed5eb4f6874d46640b
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-24 21:36:05 -0300

    hooker usage

--- pages/404.js
+++ pages/404.js
@@ -2,14 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/404.module.css'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import en from '../public/locales/en/en'
-import pt from '../public/locales/pt/pt'
+import useTranslate from './helper/useTranslate'
 
 const FourOhFour = () => {
-  const router = useRouter()
-  const { locale } = router
-  const t = locale === 'en' ? en : pt
+  const translate = useTranslate()
 
   return (
     <>
@@ -18,8 +14,8 @@ const FourOhFour = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={styles.container}>
-        <h2>{t.h2404}</h2>
-        <Link href="/">{t.link404}</Link>
+        <h2>{translate.h2404}</h2>
+        <Link href="/">{translate.link404}</Link>
         <div className={styles.image_card}>
           <Image src={'/assets/404-statue.png'} layout="fill" />
         </div>
