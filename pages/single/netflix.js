48573f27c75f705140453de84a9d094916ce0d8b
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-25 16:47:29 -0300

    i18n netflix page

--- pages/single/netflix.js
+++ pages/single/netflix.js
@@ -4,19 +4,11 @@ import styles from '../../styles/SingleProject.module.css'
 import Link from 'next/link'
 import Button from '../components/Button'
 import Head from 'next/head'
+import useTranslate from '../helper/useTranslate'
 
 const transition = {
   duration: 1,
   ease: [0.43, 0.13, 0.23, 0.96]
-}
-
-const imageVariants = {
-  exit: { y: '50%', opacity: 0, transition },
-  enter: {
-    y: '0%',
-    opacity: 1,
-    transition
-  }
 }
 
 const backVariants = {
@@ -25,6 +17,9 @@ const backVariants = {
 }
 
 const netflix = () => {
+
+const translate = useTranslate();
+
   return (
     <>
     <Head>
@@ -55,37 +50,35 @@ const netflix = () => {
             }}
           >
             <h1>Netflix</h1>
-            <h2>A simple clone of Netflix's website.</h2>
+            <h2>{translate.neth2}</h2>
           </motion.div>
           <motion.div className={styles.back} variants={backVariants}>
-            <Link href="/Projects">← Back to projects</Link>
+            <Link href="/Projects">{translate.backtoprojects}</Link>
           </motion.div>
         </motion.div>
       </AnimatePresence>
       <div className={styles.description}>
-        <h1>about the project: Netflix</h1>
+        <h1>{translate.neth1}</h1>
         <p>
-          This is a solution to the challenge proposed in DIO's Inter Bootcamp
-          to recreate Netflix's initial interface. The interface is not 100%
-          faithful to the original, we were free to adapt as we wanted.
+          {translate.netp}
         </p>
-        <h2>Features</h2>
-        <p>✔️ Carousel | ✔️ Responsiveness</p>
-        <h2>Techs Used</h2>
+        <h2>{translate.features}</h2>
+        <p>{translate.netfeats}</p>
+        <h2>{translate.techsused}</h2>
         <p>HTML | CSS | Vanilla Javascript | Jquery</p>
         <div className={styles.buttons}>
         <a
                 href="https://netflix-alike.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
-              ><Button>See it Live!</Button></a>
+              ><Button>{translate.btnSingle}</Button></a>
           <a
                 href="https://github.com/ygorrx/clone-netflix"
                 target="_blank"
                 rel="noopener noreferrer"
               ><Button>Github Repo</Button></a>
         </div>
-        <h2>Some screenshots:</h2>
+        <h2>{translate.screenS}</h2>
       </div>
       <div className={styles.img_container}>
         <div className={styles.image_card}>
