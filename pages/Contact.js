05d0a72729f223751b24e71c34a49a31f6c3c7c4
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-24 21:40:30 -0300

    translate update

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,14 +2,10 @@ import styles from '../styles/Contact.module.css'
 import Button from './components/Button'
 import Image from 'next/image'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import en from '../public/locales/en/en'
-import pt from '../public/locales/pt/pt'
+import useTranslate from './helper/useTranslate'
 
 const Contact = () => {
-  const router = useRouter()
-  const { locale } = router
-  const t = locale === 'en' ? en : pt
+  const translate = useTranslate()
 
   return (
     <>
@@ -20,8 +16,8 @@ const Contact = () => {
       <div className={`animeLeft ${styles.container}`}>
         <div className={styles.container_wrapper}>
           <div className={styles.container_center}>
-            <h1 className="title">{t.contacth1}</h1>
-            <p>{t.contactp}</p>
+            <h1 className="title">{translate.contacth1}</h1>
+            <p>{translate.contactp}</p>
           </div>
           <div className={styles.form}>
             <form
@@ -37,29 +33,29 @@ const Contact = () => {
               <input
                 type="text"
                 name="name"
-                placeholder={t.input1}
+                placeholder={translate.input1}
                 className={styles.input}
               />
               <input
                 type="email"
                 name="email"
-                placeholder={t.input2}
+                placeholder={translate.input2}
                 className={styles.input}
               />
               <input
                 type="text"
                 name="subject"
-                placeholder={t.input3}
+                placeholder={translate.input3}
                 className={styles.input}
               />
               <textarea
                 name="message"
-                placeholder={t.input4}
+                placeholder={translate.input4}
                 className={styles.input}
                 rows="15"
               ></textarea>
               <Button type="submit" className={styles.button}>
-                {t.contact_button}
+                {translate.contact_button}
               </Button>
             </form>
           </div>
