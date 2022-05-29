8643553d31c1b66697ac57fdc59783de60381ab5
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-28 21:23:22 -0300

    responsiveness

--- pages/Projects.js
+++ pages/Projects.js
@@ -13,131 +13,131 @@ import pt from '../public/locales/pt/pt'
 const transition = { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }
 
 const Projects = () => {
-
   const router = useRouter()
   const { locale } = router
-  const t = locale === 'en' ? en : pt 
+  const t = locale === 'en' ? en : pt
 
   return (
     <>
-    <Head>
+      <Head>
         <title>Ygor Moreira | Projects</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <div className={`animeLeft ${styles.container}`}>
-      <motion.div
-        key="container"
-        className={styles.container_wrapper}
-        exit={{
-          scale: 0.5,
-          opacity: 0,
-          transition: { ...transition, duration: 1 }
-        }}
-      >
-        <div className={styles.container_center}>
-          <h1 className="title">{t.project1}</h1>
-          <p>{t.project2}</p>
-        </div>
-        <AnimatePresence exitBeforeEnter>
-        {locale === 'en'? (
-          <div className={styles.cards}>
-             {contentEnglish.map((post, key) => {
-              return (
-                <div className={styles.card} key={key}>
-                  <Link
-                    href={`/single/${post.short}`}
-                    as={`/projects/${post.short}`}
-                    passHref
-                  >
-                    <motion.div
-                      key={key}
-                      className={styles.image_card}
-                      initial={{ y: '50%', opacity: 0, scale: 0.5 }}
-                      animate={{ y: 0, opacity: 1, scale: 1 }}
-                      transition={{ duration: 0.3, ease: 'easeIn' }}
-                      whileHover={{ scale: 1.1 }}
-                      exit={{
-                        scale: 0.5,
-                        opacity: 0,
-                        transition: { duration: 1 }
-                      }}
-                    >
-                      <Image src={post.image} layout="fill" />
-                    </motion.div>
-                  </Link>
-                  <div className={styles.card_container}>
-                    <div className={styles.card_body}>
-                      <h1>{post.title}</h1>
-                      <p>{post.description}</p>
-                      <p>
-                        <span>{post.techs}</span>
-                      </p>
-                    </div>
-                    <div className={styles.buttons}>
+      </Head>
+      <div className={`animeLeft ${styles.container}`}>
+        <motion.div
+          key="container"
+          className={styles.container_wrapper}
+          exit={{
+            scale: 0.5,
+            opacity: 0,
+            transition: { ...transition, duration: 1 }
+          }}
+        >
+          <div className={styles.container_center}>
+            <h1 className="title">{t.project1}</h1>
+            <p>{t.project2}</p>
+          </div>
+          <AnimatePresence exitBeforeEnter>
+            {locale === 'en' ? (
+              <div className={styles.cards}>
+                {contentEnglish.map((post, key) => {
+                  return (
+                    <div className={styles.card} key={key}>
                       <Link
                         href={`/single/${post.short}`}
                         as={`/projects/${post.short}`}
                         passHref
                       >
-                        <Button>{t.project_button}</Button>
+                        <motion.div
+                          key={key}
+                          className={styles.image_card}
+                          initial={{ y: '50%', opacity: 0, scale: 0.5 }}
+                          animate={{ y: 0, opacity: 1, scale: 1 }}
+                          transition={{ duration: 0.3, ease: 'easeIn' }}
+                          whileHover={{ scale: 1.1 }}
+                          exit={{
+                            scale: 0.5,
+                            opacity: 0,
+                            transition: { duration: 1 }
+                          }}
+                        >
+                          <Image src={post.image} layout="fill" />
+                        </motion.div>
                       </Link>
+                      <div className={styles.card_container}>
+                        <div className={styles.card_body}>
+                          <h1>{post.title}</h1>
+                          <p>{post.description}</p>
+                          <p>
+                            <span>{post.techs}</span>
+                          </p>
+                        </div>
+                        <div className={styles.buttons}>
+                          <Link
+                            href={`/single/${post.short}`}
+                            as={`/projects/${post.short}`}
+                            passHref
+                          >
+                            <Button>{t.project_button}</Button>
+                          </Link>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              )
-            })}
-            </div>):(
+                  )
+                })}
+              </div>
+            ) : (
               <div className={styles.cards}>
-              {contentPT.map((post, key) => {
-               return (
-                 <div className={styles.card} key={key}>
-                   <Link
-                     href={`/single/${post.short}`}
-                     as={`/projects/${post.short}`}
-                     passHref
-                   >
-                     <motion.div
-                       key={key}
-                       className={styles.image_card}
-                       initial={{ y: '50%', opacity: 0, scale: 0.5 }}
-                       animate={{ y: 0, opacity: 1, scale: 1 }}
-                       transition={{ duration: 0.3, ease: 'easeIn' }}
-                       whileHover={{ scale: 1.1 }}
-                       exit={{
-                         scale: 0.5,
-                         opacity: 0,
-                         transition: { duration: 1 }
-                       }}
-                     >
-                       <Image src={post.image} layout="fill" />
-                     </motion.div>
-                   </Link>
-                   <div className={styles.card_container}>
-                     <div className={styles.card_body}>
-                       <h1>{post.title}</h1>
-                       <p>{post.description}</p>
-                       <p>
-                         <span>{post.techs}</span>
-                       </p>
-                     </div>
-                     <div className={styles.buttons}>
-                       <Link
-                         href={`/single/${post.short}`}
-                         as={`/projects/${post.short}`}
-                         passHref
-                       >
-                         <Button>{t.project_button}</Button>
-                       </Link>
-                     </div>
-                   </div>
-                 </div>
-               )
-             })}
-             </div>
+                {contentPT.map((post, key) => {
+                  return (
+                    <div className={styles.card} key={key}>
+                      <Link
+                        href={`/single/${post.short}`}
+                        as={`/projects/${post.short}`}
+                        passHref
+                      >
+                        <motion.div
+                          key={key}
+                          className={styles.image_card}
+                          initial={{ y: '50%', opacity: 0, scale: 0.5 }}
+                          animate={{ y: 0, opacity: 1, scale: 1 }}
+                          transition={{ duration: 0.3, ease: 'easeIn' }}
+                          whileHover={{ scale: 1.1 }}
+                          exit={{
+                            scale: 0.5,
+                            opacity: 0,
+                            transition: { duration: 1 }
+                          }}
+                        >
+                          <Image src={post.image} layout="fill" />
+                        </motion.div>
+                      </Link>
+                      <div className={styles.card_container}>
+                        <div className={styles.card_body}>
+                          <h1>{post.title}</h1>
+                          <p>{post.description}</p>
+                          <p>
+                            <span>{post.techs}</span>
+                          </p>
+                        </div>
+                        <div className={styles.buttons}>
+                          <Link
+                            href={`/single/${post.short}`}
+                            as={`/projects/${post.short}`}
+                            passHref
+                          >
+                            <Button>{t.project_button}</Button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
             )}
-        </AnimatePresence>
-      </motion.div>
-    </div>
+          </AnimatePresence>
+        </motion.div>
+      </div>
     </>
   )
 }
