dc12cb53f244b359e3129fb689037a1456ed8532
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-05-29 22:21:09 -0300

    exit changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import React from 'react'
 import Layout from './components/Layout'
 import Loading from './components/Loading'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }) {
   const [loading, setLoading] = React.useState(true)
@@ -15,7 +16,29 @@ function MyApp({ Component, pageProps, router }) {
         <Loading />
       ) : (
         <Layout>
-          <Component {...pageProps} key={router.asPath} />
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={router.asPath}
+              animate={{
+                x: 0,
+                opacity: 1
+              }}
+              initial={{
+                x: 100,
+                opacity: 0
+              }}
+              exit={{
+                opacity: 0,
+                ease: 'easeOut'
+              }}
+              transition={{
+                duration: 0.4,
+                ease: 'easeIn'
+              }}
+            >
+              <Component {...pageProps} key={router.asPath} />
+            </motion.div>
+          </AnimatePresence>
         </Layout>
       )}
     </>
